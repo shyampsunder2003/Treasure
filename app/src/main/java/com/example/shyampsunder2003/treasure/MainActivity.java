de4490bab3e42b8a9045a1032ec2992b71f9189b
Shyam <shyampsunder2003@yahoo.com>
2015-01-29 18:48:25 +0530

    Few changes

--- app/src/main/java/com/example/shyampsunder2003/treasure/MainActivity.java
+++ app/src/main/java/com/example/shyampsunder2003/treasure/MainActivity.java
@@ -5,40 +5,76 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.Parse;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    TextView clueStatus;
+    EditText editPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        clueStatus=(TextView) findViewById(R.id.textView2);
+        editPassword=(EditText) findViewById(R.id.editText);
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "A2bfZu7LOncINvmg1TEfLBUZe9eZ0BjvedsuXq9e", "aFSDZ9JlxcbLVDZ4bj9N1Y8YrGdQ6VvOrHDX1zgR");
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("TestObject");
-        query.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> parseObjects, com.parse.ParseException e) {
-                if (e == null) {
-                    Log.d("score", "Retrieved " + parseObjects.size() + " scores");
-                } else {
-                    Log.d("score", "Error: " + e.getMessage());
-                }
-            }
-        });
+
 
     }
     //Just a little change
 
+    public boolean isOnline() {
 
+        Runtime runtime = Runtime.getRuntime();
+        try {
+
+            Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
+            int     exitValue = ipProcess.waitFor();
+            return (exitValue == 0);
+
+        } catch (IOException e)          { e.printStackTrace(); }
+        catch (InterruptedException e) { e.printStackTrace(); }
+
+        return false;
+    }
+    public void downloadClues(View view)
+    {
+        if(isOnline()) {
+            Log.d("score", "Start");
+            ParseQuery<ParseObject> query = ParseQuery.getQuery("TestObject");
+            query.findInBackground(new FindCallback<ParseObject>() {
+                @Override
+                public void done(List<ParseObject> parseObjects, com.parse.ParseException e) {
+                    if (e == null) {
+                        Log.d("score", "Retrieved " + parseObjects.size() + " scores");
+                        clueStatus.setText("Completed");
+                    } else {
+                        Log.d("score", "Error: " + e.getMessage());
+                        Toast.makeText(getApplicationContext(), e.getMessage().toString(),Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+        else
+        {
+            Toast.makeText(this,"Please check your Internet Connection",Toast.LENGTH_LONG).show();
+        }
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
